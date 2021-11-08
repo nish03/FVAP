cd96a9dfacde5e1ac718c39b3fec6e7eec38021a
Tobias Hänel <contact@tobias-haenel.de>
2021-11-08 16:59:23 +0100

    Adapt CelebA file parsing
    Former-commit-id: fe74a9e05c9e583675c62c5f21324d2687b9d3bf [formerly e15293b37b6cd17f174fb87f391b9611e5d38725]
    Former-commit-id: 72daec5de71f8ed977397ab84441e6faf3e37380

--- fair_hpo_smac/facial_attribute_prediction/CelebA.py
+++ fair_hpo_smac/facial_attribute_prediction/CelebA.py
@@ -1,10 +1,11 @@
 from csv import reader
-from itertools import islice
 from pathlib import Path
 
-from torch import tensor
+from torch import arange, tensor
 from torch.utils.data import Dataset
 from torchvision.io import read_image
+from numpy import loadtxt
+import numpy
 
 
 class CelebA(Dataset):
@@ -14,7 +15,6 @@ class CelebA(Dataset):
         split_name="all",
         image_transform=None,
         attribute_transform=None,
-        in_memory=False,
     ):
         dataset_dir_path = Path(dataset_dir_path)
         if not dataset_dir_path.is_dir():
@@ -24,65 +24,58 @@ class CelebA(Dataset):
         self.image_dir_path = dataset_dir_path / "img_align_celeba"
         self.partitions_file_path = dataset_dir_path / "list_eval_partition.txt"
         self.attribute_data_file_path = dataset_dir_path / "list_attr_celeba.txt"
-        self.partition_indices = []
-        self.image_file_paths = []
-        self.attribute_data = []
-        with open(self.partitions_file_path, "r") as partitions_file:
-            partitions_reader = reader(partitions_file, delimiter=" ")
-            for (image_file_name, partition_index) in partitions_reader:
-                self.partition_indices.append(int(partition_index))
-                self.image_file_paths.append(self.image_dir_path / image_file_name)
+
+        self.attribute_data = loadtxt(
+            "datasets/CelebA/celeba/list_attr_celeba.txt",
+            dtype=numpy.int64,
+            skiprows=2,
+            usecols=range(1, 41),
+        )
         with open(self.attribute_data_file_path, "r") as attribute_data_file:
             attribute_data_reader = reader(attribute_data_file, delimiter=" ")
-            for attribute_row in islice(attribute_data_reader, 2, None):
-                attribute_row = [
-                    attribute_value
-                    for attribute_value in attribute_row
-                    if attribute_value != ""
-                ]
-                self.attribute_data.append(
-                    [
-                        1 if attribute_row[i] == "1" else 0
-                        for i in range(1, len(attribute_row))
-                    ]
-                )
 
+            self.dataset_image_count = int(next(attribute_data_reader)[0])
+            self.attribute_names = next(attribute_data_reader)[:-1]
+
+        self.image_file_paths = []
+        self.split_image_indices = None
+        self.image_file_numbers, self.partition_indices = loadtxt(
+            str(self.partitions_file_path),
+            dtype=numpy.int64,
+            converters={0: lambda image_file_name: int(image_file_name.split(b".")[0])},
+            unpack=True,
+        )
+        self.attribute_data = loadtxt(
+            str(self.attribute_data_file_path),
+            dtype=numpy.int64,
+            skiprows=2,
+            usecols=range(1, 41),
+        )
+        self.attribute_data[self.attribute_data == -1] = 0
+        self.split(split_name)
         self.image_transform = image_transform
         self.attribute_transform = attribute_transform
 
-        self.split(split_name)
-
-        self.image_data = []
-        self.in_memory = in_memory
-        if self.in_memory:
-            self.image_data = [
-                self.load_image(i) for i in range(len(self.image_file_paths))
-            ]
-
     def __len__(self):
-        return len(self.image_file_paths)
-
-    def load_image(self, index):
-        image = read_image(str(self.image_file_paths[index]))
-        if self.image_transform:
-            image = self.image_transform(image)
-        return image
+        return self.split_image_indices.shape[0]
 
     def __getitem__(self, index):
-        image = self.image_data[index] if self.in_memory else self.load_image(index)
-        attribute_values = tensor(self.attribute_data[index])
+        image_index = self.split_image_indices[index]
+        image_file_path = (
+            self.image_dir_path / f"{self.image_file_numbers[image_index]:0>6}.jpg"
+        )
+        image = read_image(str(image_file_path))
+        attribute_values = tensor(self.attribute_data[image_index])
         if self.image_transform:
-            attribute_values = self.attribute_transform(attribute_values)
+            image = self.image_transform(attribute_values)
         if self.attribute_transform:
             attribute_values = self.attribute_transform(attribute_values)
         return image, attribute_values
 
     def split(self, split_name):
-        split_partition_indices = {"train": 0, "valid": 1, "test": 2}
-        if split_name not in split_partition_indices:
-            return
-        split_image_file_paths = []
-        for image_index, partition_index in enumerate(self.partition_indices):
-            if partition_index == split_partition_indices[split_name]:
-                split_image_file_paths.append(self.image_file_paths[image_index])
-        self.image_file_paths = split_image_file_paths
+        split_name_to_partition_index = {"train": 0, "valid": 1, "test": 2}
+        self.split_image_indices = arange(self.dataset_image_count)
+        if split_name in split_name_to_partition_index:
+            self.split_image_indices = self.split_image_indices[
+                self.partition_indices == split_name_to_partition_index[split_name]
+            ]
