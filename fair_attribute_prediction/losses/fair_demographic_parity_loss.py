d982a1f45a03d070c87a522a471a4b30dd8d2af1
Li <mengze.li@zeiss.com>
2022-01-31 15:28:19 +0100

    modify demographic loss
    Former-commit-id: 4b9e47472138647054667d059ad8f04e5d1913a4 [formerly ae3126961df65e7bf5d2ed75a89fc9aedac7661e]
    Former-commit-id: f96aaff2e8e9d0d51519fcb735bfdca7f6ae11dc

--- fair_attribute_prediction/losses/fair_demographic_parity_loss.py
+++ fair_attribute_prediction/losses/fair_demographic_parity_loss.py
@@ -14,20 +14,17 @@ def fair_demographic_parity_loss(
     target_attribute: Attribute,
 ) -> torch.Tensor:
     sensitive_attribute_targets = multi_attribute_targets[:, sensitive_attribute.index]
-    target_predictions = model.module.multi_attribute_predictions(multi_output_class_logits)[:, target_attribute.index]
+    class_probabilities = model.module.attribute_class_probabilities(multi_output_class_logits, target_attribute.index)
     demographic_loss = 0
     for target_attribute_class in range(target_attribute.size):
-        where_target_class = target_predictions.eq(target_attribute_class)
-        frequency_target_class = where_target_class.sum()
-        p_target_class = frequency_target_class/target_predictions.size(0)
+        target_attribute_probabilities = class_probabilities[:, target_attribute_class]
+        p_target_attribute = target_attribute_probabilities.mean()
         for sensitive_attribute_class in range(sensitive_attribute.size):
             where_sensitive_class = sensitive_attribute_targets.eq(sensitive_attribute_class)
-            frequency_sensitive_class = where_sensitive_class.sum()
-            index = where_target_class & where_sensitive_class
-            frequency_target_class_sensitive_class = index.sum()
+            target_attribute_probabilities_sensitive_attribute = target_attribute_probabilities[where_sensitive_class]
+            p_conditioned = target_attribute_probabilities_sensitive_attribute.mean()
             try:
-                p_conditioned = frequency_target_class_sensitive_class/frequency_sensitive_class
-                demographic_loss = demographic_loss + (p_conditioned - p_target_class).pow(2)
+                demographic_loss = demographic_loss + (p_conditioned - p_target_attribute).pow(2)
             except:
                 pass
     return tensor(demographic_loss)
