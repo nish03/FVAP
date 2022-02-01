5940ce49cc0670aa86562f39d2ca5757d3816af7
Erdem Ünal <erdem.unal96@gmail.com>
2022-02-01 14:53:48 +0100

    Equality of odds
    Former-commit-id: 94d52c8f757ec3372aad287f62b2d863c73bfcb1 [formerly fc2737677343d61a2eb123cc4e283c959b70c53d]
    Former-commit-id: a784d673a49fe64497dfff81308d925bdbb4b363

--- fair_attribute_prediction/losses/fair_equalized_odds_loss.py
+++ fair_attribute_prediction/losses/fair_equalized_odds_loss.py
@@ -1,15 +1,28 @@
 from typing import List
 
 import torch
+from torch import tensor
 
 from multi_attribute_dataset import Attribute
-
-
+    
 def fair_equalized_odds_loss(
     model: torch.nn.Module,
     multi_output_class_logits: List[torch.Tensor],
     multi_attribute_targets: torch.Tensor,
     sensitive_attribute: Attribute,
-    target_attribute: Attribute,
-) -> torch.Tensor:
-    pass
+    target_attribute: Attribute) -> torch.Tensor:
+    sensitive_attribute_targets = multi_attribute_targets[:, sensitive_attribute.index]
+    class_probabilities = model.module.attribute_class_probabilities(multi_output_class_logits, target_attribute.index)
+    attribute_targets = multi_attribute_targets[:, target_attribute.index]
+    FPP_differences = []
+    for target_attribute_class in range(target_attribute.size):
+        where_target_class = attribute_targets.eq(target_attribute_class)
+        target_attribute_probabilities = class_probabilities[:, where_target_class] 
+        p_target_ground = target_attribute_probabilities.mean() # p(y|y*)
+        for sensitive_attribute_class in range(sensitive_attribute.size):
+            where_sensitive_class = sensitive_attribute_targets.eq(sensitive_attribute_class)
+            target_attribute_probabilities_sensitive_attribute = target_attribute_probabilities[where_sensitive_class]
+            p_target_ground_sensitive = target_attribute_probabilities_sensitive_attribute.mean() # p(y|y*,s)
+            FPP_differences.append((p_target_ground_sensitive - p_target_ground).pow(2))
+
+    return tensor(FPP_differences).mean()
