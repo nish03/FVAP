50417a8a065746781ee66239b49c308f6c389536
Tobias Hänel <contact@tobias-haenel.de>
2022-02-02 20:58:02 +0100

    Added support to start multiple experiments
    Former-commit-id: 090c9fd7089740642218a0acc7bad81903637d38 [formerly 879bf7e31e49a3bf4ccd983a81052228934c1961]
    Former-commit-id: 44877557743b7949e6d191912969970987bdcbe2

--- fair_attribute_prediction/jobs/start_experiment_jobs.sh
+++ fair_attribute_prediction/jobs/start_experiment_jobs.sh
@@ -5,13 +5,14 @@ shopt -s nullglob
 JOB_DIR=$(dirname "${0}")
 PROJECT_DIR=$(realpath "${JOB_DIR}/..")
 EXPERIMENT_SLURM_FILE="${JOB_DIR}"/experiment.slurm
-EXPERIMENT_ARGS_DIR=${1}
-EXPERIMENT_NAME=$(basename "${EXPERIMENT_ARGS_DIR}")
-EXPERIMENT_ARGS_FILES=("$EXPERIMENT_ARGS_DIR"/*.args)
-EXPERIMENT_TASK_COUNT=${#EXPERIMENT_ARGS_FILES[@]}
+for EXPERIMENT_ARGS_DIR in "${@}"; do
+  EXPERIMENT_NAME=$(basename "${EXPERIMENT_ARGS_DIR}")
+  EXPERIMENT_ARGS_FILES=("$EXPERIMENT_ARGS_DIR"/*.args)
+  EXPERIMENT_TASK_COUNT=${#EXPERIMENT_ARGS_FILES[@]}
 
-sbatch \
-  --array 0-"${EXPERIMENT_TASK_COUNT}" \
-  --job-name="${EXPERIMENT_NAME}" \
-  --chdir="${PROJECT_DIR}" \
-  "${EXPERIMENT_SLURM_FILE}"
+  sbatch \
+    --array 0-"${EXPERIMENT_TASK_COUNT}" \
+    --job-name="${EXPERIMENT_NAME}" \
+    --chdir="${PROJECT_DIR}" \
+    "${EXPERIMENT_SLURM_FILE}"
+done
