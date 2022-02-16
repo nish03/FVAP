21b54dc5ecfce94c17725e71faa74ebb3d99f015
Tobias Hänel <contact@tobias-haenel.de>
2022-02-16 19:34:32 +0100

    Added checks for batches without sensitive samples
    Former-commit-id: 6a57e579b5455040ab799d3f2530266fcb02574a [formerly 742f5f495e3d6a330b3ba0318461c92fec1103fd]
    Former-commit-id: b97f607c86af7a5b99cce164d332b214dc4ab5a3

--- fair_attribute_prediction/losses/fair_demographic_parity_loss.py
+++ fair_attribute_prediction/losses/fair_demographic_parity_loss.py
@@ -16,12 +16,17 @@ def fair_demographic_parity_loss(
     sensitive_attribute_targets = multi_attribute_targets[:, sensitive_attribute.index]
     class_probabilities = model.module.attribute_class_probabilities(multi_output_class_logits, target_attribute.index)
     demographic_loss = 0
-    for target_attribute_class in range(target_attribute.size):
-        target_attribute_probabilities = class_probabilities[:, target_attribute_class]
-        p_target_attribute = target_attribute_probabilities.mean()
-        for sensitive_attribute_class in range(sensitive_attribute.size):
-            where_sensitive_class = sensitive_attribute_targets.eq(sensitive_attribute_class)
-            target_attribute_probabilities_sensitive_attribute = target_attribute_probabilities[where_sensitive_class]
-            p_conditioned = target_attribute_probabilities_sensitive_attribute.mean()
-            demographic_loss = demographic_loss + (p_conditioned - p_target_attribute).pow(2)
+    for target_class_a in range(target_attribute.size):
+        target_class_a_probabilities = class_probabilities[:, target_class_a]
+        p_pred = target_class_a_probabilities.mean()  # p(y=a)
+        for sensitive_class_b in range(sensitive_attribute.size):
+            from_sensitive_class_b = sensitive_attribute_targets.eq(sensitive_class_b)
+            if from_sensitive_class_b.sum() == 0:
+                print(f"no samples from sensitive class b ({sensitive_class_b})")
+                return tensor(0, device=multi_attribute_targets.device)
+            target_class_a_sensitive_class_b_probabilities = target_class_a_probabilities[
+                from_sensitive_class_b
+            ]  # p(y=a|s=b)
+            p_pred_sensitive = target_class_a_sensitive_class_b_probabilities.mean()
+            demographic_loss = demographic_loss + (p_pred_sensitive - p_pred).pow(2)
     return demographic_loss
