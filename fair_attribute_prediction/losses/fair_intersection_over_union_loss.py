4b8878869d314da6232af7a4c7c177f9b425b8b1
Tobias Hänel <contact@tobias-haenel.de>
2022-02-24 18:04:37 +0100

    Fix IoU backprob not beeing performed correctly
    Former-commit-id: 7f98728895dc0c627b62b0553e5704ce86b75986 [formerly 84d0d26eef7c8b467a03aa94835a200d44c1aa2c]
    Former-commit-id: 5c1ab743baa58bdf94974e16b07897a9ee358bef

--- fair_attribute_prediction/losses/fair_intersection_over_union_loss.py
+++ fair_attribute_prediction/losses/fair_intersection_over_union_loss.py
@@ -1,3 +1,4 @@
+from itertools import combinations
 from typing import List
 
 import torch
@@ -15,16 +16,15 @@ def intersection_over_union(
     confusion_matrix = (
         class_probabilities.unsqueeze(dim=2) * one_hot(attribute_targets, attribute_size).unsqueeze(dim=1)
     ).sum(dim=0)
-    class_ious = []
+    iou = 0
     for attribute_class in range(attribute_size):
-        class_iou = confusion_matrix[attribute_class, attribute_class] / (
+        iou = iou + confusion_matrix[attribute_class, attribute_class] / (
             confusion_matrix[attribute_class, :].sum()
             + confusion_matrix[:, attribute_class].sum()
             - confusion_matrix[attribute_class, attribute_class]
         )
-        class_ious.append(class_iou)
-    _iou = tensor(class_ious).mean()
-    return _iou
+    iou /= attribute_size
+    return iou
 
 
 def sensitive_intersection_over_union(
@@ -34,8 +34,8 @@ def sensitive_intersection_over_union(
 ) -> torch.Tensor:
     class_probabilities = target_class_probabilities[from_sensitive_class]
     attribute_targets = target_attribute_targets[from_sensitive_class]
-    _sensitive_iou = intersection_over_union(class_probabilities, attribute_targets)
-    return _sensitive_iou
+    sensitive_iou = intersection_over_union(class_probabilities, attribute_targets)
+    return sensitive_iou
 
 
 def fair_intersection_over_union_loss(
@@ -50,24 +50,20 @@ def fair_intersection_over_union_loss(
         multi_output_class_logits, target_attribute.index
     )
     target_attribute_targets = multi_attribute_targets[:, target_attribute.index]
-    squared_iou_differences = []
-    for sensitive_class_a in range(sensitive_attribute.size):
+    fair_iou_loss = 0.0
+    for sensitive_class_a, sensitive_class_b in combinations(range(sensitive_attribute.size), 2):
         from_sensitive_class_a = sensitive_attribute_targets.eq(sensitive_class_a)
-        if from_sensitive_class_a.sum() == 0:
+        from_sensitive_class_b = sensitive_attribute_targets.eq(sensitive_class_b)
+        if from_sensitive_class_a.sum() == 0 or from_sensitive_class_b.sum() == 0:
+            print(f"no samples for sensitive class combination ({sensitive_class_a}, {sensitive_class_b})")
             return tensor(0.0, device=sensitive_attribute_targets.device)
-        sensitive_iou_a = sensitive_intersection_over_union(
+        iou_a = sensitive_intersection_over_union(
             from_sensitive_class_a, target_class_probabilities, target_attribute_targets
         )
-        for sensitive_class_b in range(sensitive_class_a):
-            from_sensitive_class_b = sensitive_attribute_targets.eq(sensitive_class_b)
-            if from_sensitive_class_b.sum() == 0:
-                return tensor(0.0, device=sensitive_attribute_targets.device)
-            sensitive_iou_b = sensitive_intersection_over_union(
-                from_sensitive_class_b, target_class_probabilities, target_attribute_targets
-            )
+        iou_b = sensitive_intersection_over_union(
+            from_sensitive_class_b, target_class_probabilities, target_attribute_targets
+        )
+        fair_iou_loss = fair_iou_loss + (iou_a - iou_b).pow(2)
 
-            squared_iou_difference = (sensitive_iou_a - sensitive_iou_b).pow(2)
-            squared_iou_differences.append(squared_iou_difference)
-
-    _fair_iou_loss = tensor(squared_iou_differences).mean()
-    return _fair_iou_loss
+    fair_iou_loss /= sensitive_attribute.size * (sensitive_attribute.size - 1) / 2
+    return fair_iou_loss
