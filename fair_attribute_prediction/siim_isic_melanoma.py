a299aa80832d41b6cdd02621b36f715e7ed2e48a
Tobias Hänel <contact@tobias-haenel.de>
2022-03-03 21:05:08 +0100

    Added data loading for SIIM-MSIC-Melanoma dataset
    Former-commit-id: 843ff442e27ebbcce945ae2e18a009888da7459b [formerly b55d08d4224d682ce717987f835f1dac053b0c88]
    Former-commit-id: 014cf380200b39d739416e444de21916a3b6fe5b

--- fair_attribute_prediction/siim_isic_melanoma.py
+++ fair_attribute_prediction/siim_isic_melanoma.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 import torch
-from torch import Generator, randperm, tensor, arange
+from torch import Generator, randperm, tensor
 from torchvision.io import read_image
+from pandas import read_csv
 
 from multi_attribute_dataset import MultiAttributeDataset
 
@@ -15,27 +16,26 @@ class SIIMISICMelanoma(MultiAttributeDataset):
         attribute_transform=None,
         split_name="all",
         split_seed=42,
-        split_valid_test_ratio=0.5,
+        split_train_factor=0.7,
+        split_valid_factor=0.2,
     ):
-        MultiAttributeDataset.__init__(self, ["sex", "age", "diagnosis"], [2, 5, 2])
+        MultiAttributeDataset.__init__(self, ["age", "gender", "diagnosis"], [5, 2, 2])
         self.dataset_dir_path = Path(dataset_dir_path)
         if not self.dataset_dir_path.is_dir():
             raise ValueError(f"Invalid dataset directory path {dataset_dir_path} - does not exist")
-        self.split_name = split_name
+        image_directory_path = dataset_dir_path / "jpeg" / "train"
+        data_file_path = dataset_dir_path / "train.csv"
+        self.image_file_paths = sorted(image_directory_path.glob("*.jpg"))
+        self.attribute_data = read_csv(data_file_path).sort_values(by="image_name")
         self.image_transform = image_transform
         self.attribute_transform = attribute_transform
-        if not self.dataset_dir_path.is_dir():
-            raise ValueError(f"Invalid dataset directory path {dataset_dir_path} - does not exist")
-        train_image_directory_path = dataset_dir_path / "jpeg" / "train"
-        test_image_directory_path = dataset_dir_path / "jpeg" / "test"
-        train_image_file_paths = sorted(train_image_directory_path.glob("*.jpg"))
-        test_image_file_paths = sorted(test_image_directory_path.glob("*.jpg"))
-        self.image_file_paths = train_image_file_paths + test_image_file_paths
-        image_range = range(len(self.image_file_paths))
-        train_image_range = range(len(train_image_file_paths))
+        self.split_name = split_name
+        self.image_file_indices = randperm(len(self.image_file_paths), generator=Generator().manual_seed(split_seed))
+        image_range = range(len(self.image_file_indices))
+        train_image_range = range(int(split_train_factor * len(image_range)))
         valid_image_range = range(
             train_image_range.stop,
-            train_image_range.stop + int(split_valid_test_ratio * len(test_image_file_paths)),
+            train_image_range.stop + int(split_valid_factor * len(image_range)),
         )
         test_image_range = range(valid_image_range.stop, image_range.stop)
         split_image_ranges = {
@@ -44,22 +44,36 @@ class SIIMISICMelanoma(MultiAttributeDataset):
             "valid": valid_image_range,
             "test": test_image_range,
         }
-        self.image_file_indices = arange(len(self.image_file_paths))[split_image_ranges[self.split_name]]
-        self.image_file_indices = self.image_file_indices[
-            randperm(len(self.image_file_indices), generator=Generator().manual_seed(split_seed))
-        ]
-        self.attribute_data = []
+        self.image_file_indices = self.image_file_indices[split_image_ranges[self.split_name]]
 
     def __len__(self) -> int:
         return len(self.image_file_indices)
 
     def __getitem__(self, index: int) -> (torch.Tensor, torch.Tensor):
-        image_file_index = self.image_file_indices[index]
+        image_file_index = self.image_file_indices[index].item()
         image_file_path = self.image_file_paths[image_file_index]
         image = read_image(str(image_file_path))
         if self.image_transform:
             image = self.image_transform(image)
-        attribute_values = tensor([1, 2, 0])
+        sex, age_approx, diagnosis = self.attribute_data.iloc[image_file_index, [2, 3, 7]]
+        gender = torch.nan
+        if sex == "male":
+            gender = 0
+        elif sex == "female":
+            gender = 1
+        age = torch.nan
+        if age_approx <= 20:
+            age = 0
+        elif 21 <= age_approx <= 40:
+            age = 1
+        elif 41 <= age_approx <= 60:
+            age = 2
+        elif 61 <= age_approx <= 80:
+            age = 3
+        elif 81 <= age_approx:
+            age = 4
+
+        attribute_values = tensor([age, gender, diagnosis])
         if self.attribute_transform:
             attribute_values = self.target_transform(attribute_values)
         return image, attribute_values
