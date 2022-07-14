c352aa01067c358e040dfc5df79babe09709273d
Tobias Hänel <contact@tobias-haenel.de>
2022-07-14 15:38:49 +0200

    Removed old project directories
    Former-commit-id: 1f66bf6198b70321d268cd600b41fd49014f1a92 [formerly 8736f72964f48262fcc5b5b9b670cce0265a219b]
    Former-commit-id: 9e1dc27da830ae94bd61b737094c93a42f8d672a

--- fair_hpo_smac/jobs/load_alpha_modules.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-module load \
-  modenv/hiera \
-  GCC/10.2.0 \
-  CUDA/11.1.1 \
-  OpenMPI/4.0.5 \
-  Python/3.8.6
--- fair_hpo_smac/jobs/update_experiment_jobs.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/sh
-
-JOBS_DIR=$(dirname $0)
-
-rm -rf ${JOBS_DIR}/experiments
-find config -iname "*.args" | xargs python3 ${JOBS_DIR}/generate_experiment_jobs.py
